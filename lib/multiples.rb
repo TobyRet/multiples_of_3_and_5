fa50e6e96cde9c4612170d71d0ce198bd08b5e15
Toby Retallick <toby.retallick@gmail.com>
2014-02-26 12:54:33 +0000

    Refactored code...Part 1.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,35 +1,51 @@
-@natural_numbers = [1,2,3,4,5,6,7,8,9]
+#@natural_numbers = [1,2,3,4,5,6,7,8,9]
+#
+#
+#def list(number)
+#  natural_numbers = []
+#  for number in 1..1000
+#    natural_numbers << number
+#  end
+#  natural_numbers
+#  @natural_numbers = natural_numbers
+#end
+#
+#def multiples_of_three(array)
+#  array.map {|i| i % 3 == 0}
+#end
+#
+#def multiples_of_five(array)
+#  array.map {|i| i % 3 == 0}
+#end
+#
+#def add_threes(array)
+#  array.inject(0) {|sum, value| sum + value}
+#end
+#
+#def add_fives(array)
+#  array.inject(0) {|sum, value| sum + value}
+#end
+#
+#def add_threes_and_fives(array1, array2)
+#  v1 = array1.inject(0) {|sum, value| sum + value}
+#  puts v1
+#  v2 = array2.inject(0) {|sum, value| sum + value}
+#  v1 + v2
+#end
 
-
-def list(number)
-  natural_numbers = []
-  for number in 1..1000
-    natural_numbers << number
+def totaliser(max)
+  list = []
+  filtered_list = []
+  for num in 1..max 
+    list << num
   end
-  natural_numbers
-  @natural_numbers = natural_numbers
+  list.each do |i| 
+    filtered_list << i if (i % 3 == 0 || i % 5 == 0)
+  end
+  filtered_list.inject(0) {|sum, value| sum + value}
 end
 
-def multiples_of_three(array)
-  array.map {|i| i % 3 == 0}
-end
+puts totaliser(1000)
 
-def multiples_of_five(array)
-  array.map {|i| i % 3 == 0}
-end
 
-def add_threes(array)
-  array.inject(0) {|sum, value| sum + value}
-end
-
-def add_fives(array)
-  array.inject(0) {|sum, value| sum + value}
-end
-
-def add_threes_and_fives(array1, array2)
-  v1 = array1.inject(0) {|sum, value| sum + value}
-  puts v1
-  v2 = array2.inject(0) {|sum, value| sum + value}
-  v1 + v2
-end
 
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -7,34 +7,39 @@ require 'multiples'
 
 describe "Multiple Counter" do  
 
-  it 'lists the natural numbers below 1000' do
+  xit 'lists the natural numbers below 1000' do
     expect(list(1000)).to be_true
   end
 
-  it 'returns multiples of 3 from array' do
+  xit 'returns multiples of 3 from array' do
     array = [1,2,3,4,5,6,7,8,9]
     expect(multiples_of_three(array)).to be_true
   end
 
-  it 'returns multiples of 5 from an array' do
+  xit 'returns multiples of 5 from an array' do
     array = [1,2,3,4,5,6,7,8,9]
     expect(multiples_of_five(array)).to be_true
   end
 
-  it 'adds multiples of 3 from an array of values' do
+  xit 'adds multiples of 3 from an array of values' do
     array = [3,6,9]
     expect(add_threes(array)).to eq(18)
   end
 
-  it 'adds multiples of 5 from an array of values' do
+  xit 'adds multiples of 5 from an array of values' do
     array = [5,10,15]
     expect(add_fives(array)).to eq(30)
   end
 
-  it 'adds 3s and 5s from arrays' do
+  xit 'adds 3s and 5s from arrays' do
     array3 = [3,6,9]
     array5 = [5,10,15]
     expect(add_threes_and_fives(array3, array5)).to eq(48)
+  end
+
+  it 'adds multiples of threes and fives from a given list' do
+    max = 10
+    expect(totaliser(max)).to eq(33)
   end
 
 end
